a3beda4a2c7d38659e704d72d63536faf13da676
Salokhiddin <usmonovsalokhiddin@gmail.com>
2025-02-26 07:26:19 +0500

    user app updated

--- fasttracks/users/models.py
+++ fasttracks/users/models.py
@@ -6,7 +6,9 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email: str, password: str, **extra_fields):
-        if not email:raise ValueError(_("The Email must be set"))
+        if not email: raise ValueError(_("The email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        
+        user.set_password(password)
+        user.save()
+        return user
